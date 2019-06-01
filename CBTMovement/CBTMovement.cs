30b4e08d611ec51a14d0c42799916129f45f7678
Madranis <madranis@example.com>
2019-06-01 15:28:08 -0400

    Remove permanent evasion portion of the mod and rename this fork of CBTMovement to CBTMovementCore.
    Allows the movement modifiers and action after sprint features while allowing
    choice of evasion behavior.  Use the base game default or use a more
    feature-rich mod like semi-permanent evasion to get the desired evasion
    behavior

--- CBTMovement/CBTMovement.cs
+++ CBTMovement/CBTMovement.cs
@@ -95,45 +95,6 @@ namespace CBTMovement
     }
 
 
-    [HarmonyPatch(typeof(AbstractActor), "ResolveAttackSequence", null)]
-    public static class AbstractActor_ResolveAttackSequence_Patch
-    {
-        private static bool Prefix(AbstractActor __instance)
-        {
-            return false;
-        }
-
-        private static void Postfix(AbstractActor __instance, string sourceID, int sequenceID, int stackItemID, AttackDirection attackDirection)
-        {
-            AttackDirector.AttackSequence attackSequence = __instance.Combat.AttackDirector.GetAttackSequence(sequenceID);
-            if (attackSequence != null && attackSequence.attackDidDamage)
-            {
-                List<Effect> list = __instance.Combat.EffectManager.GetAllEffectsTargeting(__instance).FindAll((Effect x) => x.EffectData.targetingData.effectTriggerType == EffectTriggerType.OnDamaged);
-                for (int i = 0; i < list.Count; i++)
-                {
-                    list[i].OnEffectTakeDamage(attackSequence.attacker, __instance);
-                }
-                if (attackSequence.isMelee)
-                {
-                    int value = attackSequence.attacker.StatCollection.GetValue<int>("MeleeHitPushBackPhases");
-                    if (value > 0)
-                    {
-                        for (int j = 0; j < value; j++)
-                        {
-                            __instance.ForceUnitOnePhaseDown(sourceID, stackItemID, false);
-                        }
-                    }
-                }
-            }
-            int evasivePipsCurrent = __instance.EvasivePipsCurrent;
-            int evasivePipsCurrent2 = __instance.EvasivePipsCurrent;
-            if (evasivePipsCurrent2 < evasivePipsCurrent && !__instance.IsDead && !__instance.IsFlaggedForDeath)
-            {
-                __instance.Combat.MessageCenter.PublishMessage(new FloatieMessage(__instance.GUID, __instance.GUID, "-1 EVASION", FloatieMessage.MessageNature.Debuff));
-            }
-        }
-    }
-
     internal class ModSettings
     {
         [JsonProperty("ToHitSelfJumped")]
